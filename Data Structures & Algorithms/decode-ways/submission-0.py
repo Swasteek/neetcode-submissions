a1cbf4aa78b88488e83cf3967ad09edcad35fe34
Swasteek <143004405+Swasteek@users.noreply.github.com>
2026-07-09 16:38:38 +0530

    Add: decode-ways - submission-0

---
+++ Data Structures & Algorithms/decode-ways/submission-0.py
@@ -0,0 +1,18 @@
+class Solution:
+    def numDecodings(self, s: str) -> int:
+        n=len(s)
+        dp=[-1]*(n+2)
+        def f(i):
+            if i==n:
+                return 1
+            if s[i]=='0':
+                return 0
+            if dp[i]!=-1:
+                return dp[i]
+            res=f(i+1)
+            if i<n-1:
+                if s[i]=='1' or s[i]=='2' and s[i+1]<'7':
+                    res+=f(i+2)
+            dp[i]=res
+            return dp[i]
+        return f(0)
